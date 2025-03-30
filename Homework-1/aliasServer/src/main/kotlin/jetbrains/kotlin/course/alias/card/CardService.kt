702c120e42cb079ce8f965f95605a3643dbea87a
Ananias N. Amupadi <135354794+ananiasamupadi@users.noreply.github.com>
2025-03-30 02:52:30 +0200

    Update CardService.kt

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -3,10 +3,31 @@ package jetbrains.kotlin.course.alias.card
 import org.springframework.stereotype.Service
 
 @Service
-class CardService {
-    private fun generateCards(): List<Card> = TODO("Not implemented yet")
+class CardService(private val identifierFactory: IdentifierFactory) {
 
-    private fun List<String>.toWords(): List<Word> = TODO("Not implemented yet")
+    companion object {
+        private const val WORDS_IN_CARD = 4
+        private const val CARDS_AMOUNT = 10
+    }
 
-    fun getCardByIndex(index: Int): Card = TODO("Not implemented yet")
+    private val cards: List<Card> = generateCards()
+
+    private fun generateCards(): List<Card> {
+        val allWords = listOf("apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew")
+            .shuffled()
+            .toWords()
+
+        if (allWords.size < CARDS_AMOUNT * WORDS_IN_CARD) {
+            throw IllegalStateException("Not enough words to generate cards.")
+        }
+
+        return allWords.chunked(WORDS_IN_CARD).take(CARDS_AMOUNT).map { words ->
+            val id = identifierFactory.createIdentifier()
+            Card(id, words)
+        }
+    }
+
+    private fun List<String>.toWords(): List<Word> = map { Word(it) }
+
+    fun getCardByIndex(index: Int): Card? = cards.getOrNull(index)
 }
